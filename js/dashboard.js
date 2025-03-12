cad6fc78c5280c00a678bf85c6bb6f3d2d9eaeda
Daniel <dpaulbagube19@gmail.com>
2025-03-12 22:08:38 +0800

    Updated Dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,43 +1,188 @@
 let products = [
     {
-        name: "Vegetables", 
-        quantity: 1,
-        category:"Fruits",
+        name: "Burgir",
+        quantity: 34,
+        category: "All",
         ratings: 5,
-        price: "$10",
-        stock: "In Stock",
-        description: "Fresh organic vegetables",
+        price: 50,
+        unit: "kg",
+        stock: 45,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/PV assets/burgir.png"
+    },
+    {
+        name: "Mango",
+        quantity: 34,
+        category: "Fruits",
+        ratings: 5,
+        price: 50,
+        unit: "kg",
+        stock: 45,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/fruits/1.png"
+    },
+    {
+        name: "Strawberry",
+        quantity: 53,
+        category: "Fruits",
+        ratings: 4,
+        price: 45,
+        unit: "kg",
+        stock: 54,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/fruits/2.png"
+    },
+    {
+        name: "Rambutan",
+        quantity: 45,
+        category: "Fruits",
+        ratings: 3,
+        price: 45,
+        unit: "kg",
+        stock: 23,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/fruits/3.png"
+    },
+    {
+        name: "Carots",
+        quantity: 34,
+        category: "Vegetable",
+        ratings: 5,
+        price: 50,
+        unit: "kg",
+        stock: 45,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/vegetables/1.png"
+    },
+    {
+        name: "Talong Ni Juswa",
+        quantity: 34,
+        category: "Vegetable",
+        ratings: 10,
+        price: 10000,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/vegetables/2.png"
+    },
+    {
+        name: "Beans",
+        quantity: 23,
+        category: "Vegetable",
+        ratings: 1,
+        price: 34,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/vegetables/3.png"
+    },
+    {
+        name: "Green Onion",
+        quantity: 23,
+        category: "Crops",
+        ratings: 3,
+        price: 34,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
         image: "/assets/products/crops/1.png"
     },
     {
-        name: "Fruits", 
-        quantity: 1,
-        category:"Fruits",
+        name: "Rice",
+        quantity: 23,
+        category: "Crops",
         ratings: 4,
-        price: "$15",
-        stock: "In Stock",
-        description: "Sweet and juicy fruits",
+        price: 34,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
         image: "/assets/products/crops/2.png"
     },
     {
-        name: "Grains", 
-        quantity: 1,
-        category:"Vegetable",
-        ratings: 3,
-        price: "$8",
-        stock: "Out of Stock",
-        description: "High-quality grains",
+        name: "Mung Bean",
+        quantity: 23,
+        category: "Crops",
+        ratings: 5,
+        price: 34,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
         image: "/assets/products/crops/3.png"
     },
+    {
+        name: "Chicken Meat",
+        quantity: 23,
+        category: "Poultry",
+        ratings: 5,
+        price: 76,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/poultry/1.png"
+    },
+    {
+        name: "Salted Egg",
+        quantity: 23,
+        category: "Poultry",
+        ratings: 5,
+        price: 54,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/poultry/2.png"
+    },
+    {
+        name: "Fresh Milk",
+        quantity: 76,
+        category: "Poultry",
+        ratings: 4,
+        price: 42,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/poultry/3.png"
+    },
+    {
+        name: "Pig",
+        quantity: 34,
+        category: "Livestocks",
+        ratings: 4,
+        price: 42,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/livestocks/1.png"
+    },
+    {
+        name: "Geese",
+        quantity: 43,
+        category: "Livestocks",
+        ratings: 4,
+        price: 74,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/livestocks/2.png"
+    },
+    {
+        name: "Rabbit",
+        quantity: 23,
+        category: "Livestocks",
+        ratings: 4,
+        price: 42,
+        unit: "kg",
+        stock: 100,
+        description: "Lorem ipsum dolor sit amet consectetur. Eget sit posuere enim facilisi. Pretium orci venenatis habitasse gravida nulla tincidunt iaculis. Aliquet at massa quisque libero viverra ut sed. Est vulputate est rutrum nunc nunc pellentesque ultrices pharetra. Mauris euismod sed vel quisque tincidunt suspendisse sed turpis volutpat.",
+        image: "/assets/products/livestocks/3.png"
+    },
+
 ];
 
 let productContainer = document.getElementById('product_list');
-
-
 function displayProduct(filteredProducts) {
     let productContainer = document.getElementById('product_list');
-    productContainer.innerHTML = ''; 
-    
+    productContainer.innerHTML = '';
+
     filteredProducts.forEach(product => {
         let li = document.createElement('li');
         li.innerHTML = `
@@ -52,7 +197,7 @@ function displayProduct(filteredProducts) {
                         <p>${product.ratings} Ratings</p>
                     </div>
                     <div class="product_price">
-                        <p>${product.price}</p>
+                        <p>$${product.price}</p>
                     </div>
                 </div>
             </a>
@@ -62,16 +207,16 @@ function displayProduct(filteredProducts) {
 }
 
 displayProduct(products);
+
+//BUTTON CATEGORY
 document.querySelectorAll('.categoryBtn').forEach(button => {
-    button.addEventListener('click', function(event) {
+    button.addEventListener('click', function (event) {
         let selectedCategory = this.querySelector('img').getAttribute('data-category');
         if (selectedCategory === "All") {
-            displayProduct(products); 
+            displayProduct(products);
         } else {
             let filteredProducts = products.filter(product => product.category === selectedCategory);
             displayProduct(filteredProducts);
         }
     });
 });
-
-console.log(window.location.pathname)
